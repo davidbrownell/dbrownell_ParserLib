9b20793f5c386d9f562d34d7e94a4705a1bdb1bf
Dave Brownell <github@DavidBrownell.com>
2026-06-20 11:23:41 -0400

    ✨ [+feature] Added errors.py

--- src/dbrownell_ParserLib/__init__.py
+++ src/dbrownell_ParserLib/__init__.py
@@ -2,12 +2,15 @@
 
 from importlib.metadata import version
 
+from dbrownell_ParserLib.errors import Error, PythonError
 from dbrownell_ParserLib.location import Location
 from dbrownell_ParserLib.region import Region
 
 # ----------------------------------------------------------------------
 __all__ = [
+    "Error",
     "Location",
+    "PythonError",
     "Region",
     "__version__",
 ]
